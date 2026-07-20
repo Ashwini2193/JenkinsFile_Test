3b916ffe1cea032d4cddec03d2fdca2b17feaba4
Ashwini2193 <ashwini-venkatasubbarao-mutte.mutte@capgemini.com>
2026-07-20 12:28:40 +0530

    Update test.java

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -3,7 +3,8 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Jenkins test")
+System.out.println("Jenkins test");
+	123
 	}
 	}
 
